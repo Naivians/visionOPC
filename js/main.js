59705e93c7d752eb55af78017ed505a5bdbf46af
Naivians <madera.marvin98@gmail.com>
2024-02-27 16:36:25 +0800

    Deleting unuse JS codes

--- js/main.js
+++ js/main.js
@@ -5,19 +5,8 @@ let navLinks = document.querySelectorAll(".nav-link")
 window.addEventListener('scroll', function() {
     if(window.scrollY > 50){
         nav.classList.add('nav-danger', 'shadow')
-        
-        navLinks.forEach((tags)=>{
-            tags.classList.add('nav-black')
-        })
-
-        // navLinks.classList.add('nav-black')
     }else{
         nav.classList.remove('nav-danger', 'shadow')
-        // navLinks.classList.remove('nav-black')
-
-        navLinks.forEach((tags)=>{
-            tags.classList.remove('nav-black')
-        })
     }
 })
 
