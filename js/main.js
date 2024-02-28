152306c4223d70adf06b47691ea35070b260d9a5
Naivians <madera.marvin98@gmail.com>
2024-02-28 16:09:02 +0800

    Done home, about and contact page

--- js/main.js
+++ js/main.js
@@ -3,10 +3,37 @@
 let nav = document.querySelector("nav")
 let navLinks = document.querySelectorAll(".nav-link")
 window.addEventListener('scroll', function() {
-    if(window.scrollY > 50){
+    if(window.scrollY > 10){
         nav.classList.add('nav-danger', 'shadow')
     }else{
         nav.classList.remove('nav-danger', 'shadow')
     }
 })
 
+// animate on Scroll 
+AOS.init();
+
+// testimonials
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  
