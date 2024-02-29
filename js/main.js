08d1a53f7ea9d8126cce1d8b4d171313fe10b3ed
Naivians <madera.marvin98@gmail.com>
2024-03-01 07:32:50 +0800

    contact page is done responsive

--- js/main.js
+++ js/main.js
@@ -1,39 +1,38 @@
 // be default navbar is transparent
 // then add bg-dark when scroll > 100
-let nav = document.querySelector("nav")
-let navLinks = document.querySelectorAll(".nav-link")
-window.addEventListener('scroll', function() {
-    if(window.scrollY > 10){
-        nav.classList.add('nav-danger', 'shadow')
-    }else{
-        nav.classList.remove('nav-danger', 'shadow')
-    }
-})
+let nav = document.querySelector("nav");
+let navLinks = document.querySelectorAll(".nav-link");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 10) {
+    nav.classList.add("nav-danger", "shadow");
+  } else {
+    nav.classList.remove("nav-danger", "shadow");
+  }
+});
 
-// animate on Scroll 
+// animate on Scroll
 AOS.init();
 
 // testimonials
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
 
-    // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
 
-    // // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
-  
+  // // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    color: "white",
+  },
+});
